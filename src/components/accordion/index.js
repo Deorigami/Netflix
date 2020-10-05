ab2c5493606570696d2edd41fb33a9765925ec44
deorigami <anggaardinata2409@gmail.com>
2020-10-05 16:22:28 +0700

    adding accordion

---
+++ src/components/accordion/index.js
@@ -0,0 +1,62 @@
+import React, { useState, createContext, useContext } from "react";
+import {
+  Title,
+  Container,
+  Frame,
+  Inner,
+  Item,
+  Header,
+  Body,
+} from "./style/accordion";
+
+const ToggleContext = createContext();
+
+export default function Accordion({ children, ...restProps }) {
+  return (
+    <Container {...restProps}>
+      <Inner>{children}</Inner>
+    </Container>
+  );
+}
+
+Accordion.Title = function AccordionTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
+  return <Frame {...restProps}>{children}</Frame>;
+};
+
+Accordion.Item = function AccordionItem({ children, ...restProps }) {
+  const [toggleShow, setToggleShow] = useState(false);
+  return (
+    <ToggleContext.Provider
+      value={{ toggleShow, setToggleShow }}
+      {...restProps}
+    >
+      <Item>{children}</Item>
+    </ToggleContext.Provider>
+  );
+};
+
+Accordion.Header = function AccordionHeader({ children, ...restProps }) {
+  const { toggleShow, setToggleShow } = useContext(ToggleContext);
+  return (
+    <Header
+      onClick={() => setToggleShow((toggleShow) => !toggleShow)}
+      {...restProps}
+    >
+      {children}
+      {toggleShow ? (
+        <img src="/images/icon/close-slim.png" alt="close" />
+      ) : (
+        <img src="/images/icon/add.png" alt="open" />
+      )}
+    </Header>
+  );
+};
+
+Accordion.Body = function AccordionBody({ children, ...restProps }) {
+  const { toggleShow } = useContext(ToggleContext);
+  return toggleShow ? <Body {...restProps}>{children}</Body> : null;
+};
